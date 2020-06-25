cc6db7790e6eff0ea501e65222f81de32cd9113f
nami <krmglish@gmail.com>
2020-06-25 23:51:04 +0900

    Q8の修正、Q10に回答

--- task.rb
+++ task.rb
@@ -64,7 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages = programming_languages.map(&:capitalize)
+  programming_languages.map(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -85,16 +85,23 @@ end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
-
+  
   # 以下に回答を記載
-
+ foods.each do |food|
+   if food.include?("うに")
+    puts "好物です"
+   else
+    puts "まぁまぁ好きです"
+   end
+ end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  newsports = sports.uniq
+  p newsports
 end
 
 def q12
