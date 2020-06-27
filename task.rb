695eaf9e6782f41e7a31fe94a3b57ba8bf227bc9
nami <krmglish@gmail.com>
2020-06-27 11:51:18 +0900

    Q8.10の修正、Q11-13に回答

--- task.rb
+++ task.rb
@@ -64,7 +64,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map(&:capitalize)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -88,27 +88,26 @@ def q10
   
   # 以下に回答を記載
  foods.each do |food|
-   if food.include?("うに")
-    puts "好物です"
-   else
-    puts "まぁまぁ好きです"
-   end
- end
+   puts food.include?("うに") ? "好物です" : "まぁまぁ好きです"
+ end  
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  newsports = sports.uniq
-  p newsports
+  puts "ユーザーの趣味一覧"
+  newsports = sports.flatten.uniq
+  newsports.each.with_index(1) do |newsports, i|
+    puts "No.#{i} #{newsports}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -116,7 +115,9 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data[:age] = 32
+  user_data[:address] = "沖縄"
+  puts user_data
 end
 
 def q14
